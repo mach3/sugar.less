e9f5476d2401543233e3afa22442984fa838d72b
mach3 <mach3ss@gmail.com>
2013-01-06 18:59:52 +0900

    add align feature for grid

--- grunt.js
+++ grunt.js
@@ -1,18 +1,14 @@
 module.exports = function(grunt){
 
-	grunt.loadNpmTasks("grunt-contrib");
-
 	grunt.initConfig({
 
-		less : {
+		lessc : {
 			test : {
-				files : {
-					"test/gradient/style.css" : ["test/gradient/style.less"],
-					"test/grid/style.css" : ["test/grid/style.less"],
-					"test/fonts/style.css" : ["test/fonts/style.less"],
-					"test/css3/style.css" : ["test/css3/style.less"],
-					"test/utils/style.css" : ["test/utils/style.less"],
-				}
+				"test/gradient/style.css" : "test/gradient/style.less",
+				"test/grid/style.css" : "test/grid/style.less",
+				"test/fonts/style.css" : "test/fonts/style.less",
+				"test/css3/style.css" : "test/css3/style.less",
+				"test/utils/style.css" : "test/utils/style.less",
 			}
 		},
 
@@ -22,10 +18,32 @@ module.exports = function(grunt){
 					"less/**",
 					"test/*/style.less",
 				],
-				tasks : "less:test"
+				tasks : "lessc:test"
 			}
 		}
 
 	});
+
+	grunt.registerMultiTask(
+		"lessc",
+		"Compile less css",
+		function(){
+			var exec, data, cmd, dist, done;
+
+			exec = require("child_process").exec;
+			data = this.data;
+
+			for(dist in data){
+				cmd = ([ "lessc", data[dist], dist ]).join(" ");
+				done = this.async();
+				exec(cmd, function(e, stdout, stderr){
+					if(null != e){
+						grunt.log.error(stderr);
+					}
+					done();
+				});
+			}
+		}
+	);
 
 };
