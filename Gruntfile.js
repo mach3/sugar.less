7ce4ebcc90e64f9ba7935631d321e89b422b2290
mach3 <mach3ss@gmail.com>
2013-03-05 14:09:33 +0900

    update ignore, add components, update grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,21 +1,24 @@
-module.exports = function(grunt){
 
-	grunt.loadNpmTasks("grunt-contrib");
+
+module.exports = function(grunt){
+	
+	var cp = require("child_process");
+
+	grunt.loadNpmTasks("grunt-contrib-less");
 
 	grunt.initConfig({
-
 		less : {
 			test : {
 				files : {
+					"test/all/style.css" : ["./less/sugar.less"],
 					"test/gradient/style.css" : ["test/gradient/style.less"],
 					"test/grid/style.css" : ["test/grid/style.less"],
 					"test/fonts/style.css" : ["test/fonts/style.less"],
 					"test/css3/style.css" : ["test/css3/style.less"],
-					"test/utils/style.css" : ["test/utils/style.less"],
+					"test/utils/style.css" : ["test/utils/style.less"]
 				}
 			}
 		},
-
 		watch : {
 			test : {
 				files : [
@@ -25,7 +28,18 @@ module.exports = function(grunt){
 				tasks : "less:test"
 			}
 		}
+	});
 
+	grunt.registerTask("install", function(){
+		var done = this.async();
+		cp.exec("sh ./install.sh", function(e, out, error){
+			if(error){
+				grunt.log.error(error);
+			} else {
+				grunt.log.write(out);
+			}
+			done();
+		});
 	});
 
 };
---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+bower install
+
+cp ./components/normalize-css/normalize.css ./less/sugar/normalize.less
+
+echo ---------------------------------------
+echo Dependencies are successfully installed.
